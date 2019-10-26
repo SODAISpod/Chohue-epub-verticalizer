240098970be9501003f7503ee6b15586e50d2aab
gui <new@email.com>
2019-10-27 00:16:10 +0800

    修正轉檔後重複執行的問題

---
+++ 作夥直排/NewVersionCheck.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChoHoe
+{
+    class NewVersionCheck
+    {
+        Uri url = new Uri("http://api.github.com/repos/SODAIS69/Chohue-EpubVerticalizationer/releases/latest");
+
+       public bool Hasnew()
+        {
+            using (WebClient wc = new WebClient())
+            {
+                System.Net.ServicePointManager.ServerCertificateValidationCallback = delegate { return true; };
+              
+            //    var json = wc.DownloadString(url);
+            }
+            return true;
+        }
+
+    }
+}
