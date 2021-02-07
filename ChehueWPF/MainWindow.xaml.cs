d11511f8268b397a429c991d17c5b29897860e7f
gui <kiicho@kiicho.cc>
2021-02-08 02:21:45 +0800

    新增controls

--- ChehueWPF/MainWindow.xaml.cs
+++ ChehueWPF/MainWindow.xaml.cs
@@ -5,6 +5,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Controls.Primitives;
 using System.Windows.Data;
 using System.Windows.Documents;
 using System.Windows.Input;
@@ -34,5 +35,26 @@ namespace ChehueWPF
         {
 
         }
+    }
+
+    public class LockableToggleButton : ListBoxItem
+    {
+        protected override void OnToggle()
+        {
+            if (!LockToggle)
+            {
+                base.OnToggle();
+            }
+        }
+
+        public bool LockToggle
+        {
+            get { return (bool)GetValue(LockToggleProperty); }
+            set { SetValue(LockToggleProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for LockToggle.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty LockToggleProperty =
+            DependencyProperty.Register("LockToggle", typeof(bool), typeof(LockableToggleButton), new UIPropertyMetadata(false));
     }
 }
